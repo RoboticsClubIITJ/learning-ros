4983ad6991d71cd167207b1aedc41a779128dd5a
vedthakur <thakur.6@iitj.ac.in>
2020-08-06 01:18:53 +0530

    Added my catkin workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/vedthakur/catkin_ws/src
+# Build directory: /home/vedthakur/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
