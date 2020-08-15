bceb7b3b968dde32503b94028ccb8f749b4b35b9
vedthakur <thakur.6@iitj.ac.in>
2020-08-16 00:17:13 +0530

    Using Publisher and Subscriber in a single python file

--- control_p3dx/src/overall_control.py
+++ control_p3dx/src/overall_control.py
@@ -3,20 +3,15 @@ import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
-rospy.init_node('topic_publisher','topic_subscriber')
-pub=rospy.Publisher('/pioneer/cmd_vel',Twist,queue_size=1)
-
-rate=rospy.Rate(2)
-twist=Twist()
-
-twist.linear.x=0.5
-twist.angular.z=0.5
-
 def callback(msg):
    print(msg.pose.pose.position)
+   twist.linear.x=0.5
+   twist.angular.z=0.5
+   pub.publish(twist)
 
-while not rospy.is_shutdown():
-    pub.publish(twist)
-    sub=rospy.Subscriber('/odom',Odometry,callback)
-    rate.sleep()
+rospy.init_node('topic_publisher')
+pub=rospy.Publisher('/pioneer/cmd_vel',Twist,queue_size=1)
+twist=Twist()
+sub=rospy.Subscriber('/odom',Odometry,callback)
 
+rospy.spin()
